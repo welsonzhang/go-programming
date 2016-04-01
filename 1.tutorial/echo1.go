e3a5545b1d5a0e5ccc41f83c2d7aafbc06ec1c18
welsonzhang <942469560@qq.com>
2016-04-01 22:09:15 +0800

    update

---
+++ 1.tutorial/echo1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func main(){
+    var s, sep string
+    for i:=0; i < len(os.Args); i++ {
+        s += sep + os.Args[i]
+        sep = " "
+    }
+    fmt.Println(s)
+    
+    for i:=0; i < len(os.Args); i++ {
+        fmt.Println(i,os.Args[i])
+    }
+}
